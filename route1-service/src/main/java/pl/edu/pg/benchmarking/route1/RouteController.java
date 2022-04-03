afd874dd95b05eb332dda575b6911b49261985e7
Piotr Rogowski <rogowski.piotr99@gmail.com>
2022-04-03 14:53:43 +0200

    add route1-service implementation

---
+++ route1-service/src/main/java/pl/edu/pg/benchmarking/route1/RouteController.java
@@ -0,0 +1,26 @@
+package pl.edu.pg.benchmarking.route1;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Set;
+
+@RestController
+@RequestMapping("api/route1")
+public class RouteController {
+    private final RouteService routeService;
+
+    public RouteController(RouteService routeService) {
+        this.routeService = routeService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Integer>> computeRoute(@RequestParam Set<Integer> id) {
+        return ResponseEntity.ok(routeService.computeRoute(id));
+    }
+
+}
