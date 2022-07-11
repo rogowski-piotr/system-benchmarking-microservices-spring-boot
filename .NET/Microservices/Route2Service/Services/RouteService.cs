b0446ec04ba81aa554bd664130db2f95ad832f0a
Bartosz Miąskowski <EquablePanic4@hotmail.com>
2022-07-12 00:50:04 +0200

    Added RouteService service

---
+++ .NET/Microservices/Route2Service/Services/RouteService.cs
@@ -0,0 +1,14 @@
+﻿namespace Route2Service.Services
+{
+    public class RouteService
+    {
+        private readonly DistanceRepository _distanceRepository;
+        private readonly PlaceRepository _placeRepository;
+
+        public RouteService(DistanceRepository distanceRepository, PlaceRepository placeRepository)
+        {
+            _distanceRepository = distanceRepository;
+            _placeRepository = placeRepository;
+        }
+    }
+}
