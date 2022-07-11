89166ee95461afd4fd5d78c1bbfecb3056f1c664
Bartosz Miąskowski <EquablePanic4@hotmail.com>
2022-07-12 00:47:47 +0200

    Added DistanceRepository service using copypasting design pattern

---
+++ .NET/Microservices/Route2Service/Services/DistanceRepository.cs
@@ -0,0 +1,20 @@
+﻿using Route2Service.Models;
+
+namespace Route2Service.Services
+{
+    public class DistanceRepository
+    {
+        private readonly HttpClient _httpClient;
+
+        public DistanceRepository(string uri)
+        {
+            _httpClient = new() { BaseAddress = new(uri) };
+        }
+
+        public async Task<double> FindDistanceAsync(string cord1, string cord2)
+        {
+            var response = await _httpClient.PostAsJsonAsync("distance", new DistanceRequest { Coordinate1 = cord1, Coordinate2 = cord2 });
+            return (await response.Content.ReadFromJsonAsync<DistanceResponse>()).Distance;
+        }
+    }
+}
