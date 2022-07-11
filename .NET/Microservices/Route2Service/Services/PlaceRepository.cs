cb918647fcc73215a84e0caf441bd33df41b6259
Bartosz Miąskowski <EquablePanic4@hotmail.com>
2022-07-12 00:48:39 +0200

    Added PlaceRepository service using copypasting design pattern

---
+++ .NET/Microservices/Route2Service/Services/PlaceRepository.cs
@@ -0,0 +1,21 @@
+﻿using Route2Service.Models;
+
+namespace Route2Service.Services
+{
+    public class PlaceRepository
+    {
+        private readonly HttpClient _httpClient;
+
+        public PlaceRepository(string uri)
+        {
+            _httpClient = new() { BaseAddress = new(uri) };
+        }
+
+        public async Task<string> FindPlaceCoordinatesAsync(int placeId)
+        {
+            var response = await _httpClient.GetFromJsonAsync<PlaceResponse>($"places/{placeId}");
+
+            return response.Coordinates;
+        }
+    }
+}
