993d3b40e9885d3ced1cdb6909064a4eb0b83553
Bartosz Miąskowski <EquablePanic4@hotmail.com>
2022-07-15 20:24:28 +0200

    Benchmark configured using compilation flags

--- .NET/Benchmark/Program.cs
+++ .NET/Benchmark/Program.cs
@@ -1,1 +1,21 @@
-﻿
+﻿using Benchmark.Benchmarks;
+
+using BenchmarkDotNet.Running;
+
+#if Distance
+
+var summary = BenchmarkRunner.Run<DistanceBenchmark>();
+
+#elif Place
+
+var summary = BenchmarkRunner.Run<PlaceBenchmark>();
+
+#elif Route1
+
+var summary = BenchmarkRunner.Run<Route1Benchmark>();
+
+#elif Route2
+
+var summary = BenchmarkRunner.Run<Route2Benchmark>();
+
+#endif
