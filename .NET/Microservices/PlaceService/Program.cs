f90ad0670475d7cc84b1e8e1ee20dc1ca14af026
Bartosz Miąskowski <EquablePanic4@hotmail.com>
2022-07-15 17:06:12 +0200

    Changed Place Service design pattern from minimal API to Controller based api

--- .NET/Microservices/PlaceService/Program.cs
+++ .NET/Microservices/PlaceService/Program.cs
@@ -2,22 +2,12 @@ using PlaceService.Repositories;
 using PlaceService.Responses;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddSingleton<PlaceRepository>();
+builder.Services.AddControllers();
+
 var app = builder.Build();
 
-var repository = new PlaceRepository();
-
-//Get all places
-app.MapGet("/api/places", () => Results.Ok(PlacesResponse.GetPlacesResponses(repository.Places)));
-
-//Get single place
-app.MapGet("/api/places/{id}", (int id) =>
-{
-    var place = repository.FindById(id);
-
-    if (place == null)
-        return Results.NotFound();
-
-    return Results.Ok(new PlaceResponse(place));
-});
+app.MapControllers();
 
 app.Run();
