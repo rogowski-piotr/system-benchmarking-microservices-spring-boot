453272210dbec84e4617d404ce406ecf2723ad52
Bartosz Miąskowski <EquablePanic4@hotmail.com>
2022-07-15 19:28:52 +0200

    Implemented working Program.cs (monolith)

--- .NET/Monolith/Program.cs
+++ .NET/Monolith/Program.cs
@@ -3,6 +3,15 @@ using Monolith.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddSingleton<PlaceRepository>();
+builder.Services.AddSingleton<SphericalDistanceService>();
+builder.Services.AddSingleton<Route1Service>();
+builder.Services.AddSingleton<Route2Service>();
+
+builder.Services.AddControllers();
+
 var app = builder.Build();
+
+app.MapControllers();
 
 app.Run();
