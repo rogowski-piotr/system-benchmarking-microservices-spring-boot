d21c09df8a2c78aa41c3ca834a8c71a04e68f8cb
Bartosz Miąskowski <EquablePanic4@hotmail.com>
2022-07-15 20:13:07 +0200

    Implemented distance benchmark

--- .NET/Benchmark/Benchmarks/DistanceBenchmark.cs
+++ .NET/Benchmark/Benchmarks/DistanceBenchmark.cs
@@ -1,4 +1,10 @@
-﻿using System;
+﻿using Benchmark.Models;
+using Benchmark.Models.Requests;
+using Benchmark.Services;
+
+using BenchmarkDotNet.Attributes;
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +14,31 @@ namespace Benchmark.Benchmarks
 {
     public class DistanceBenchmark
     {
+        private SphericalDistanceService _distanceService;
+
+        private Point p1;
+        private Point p2;
+
+        [GlobalSetup]
+        public void Setup()
+        {
+            _distanceService = new();
+
+            var distanceRequest = new DistanceRequest
+            {
+                Coordinate1 = "54.367, 18.633",
+                Coordinate2 = "50.05, 19.95"
+            };
+
+            var points = DistanceRequest.DtoToEntityMapper(distanceRequest);
+            p1 = points[0];
+            p2 = points[1];
+        }
+
+        [Benchmark]
+        public double CalculateDistance()
+        {
+            return _distanceService.CalculateDistance(p1, p2);
+        }
     }
 }
