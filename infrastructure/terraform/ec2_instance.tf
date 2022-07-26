a77e7f063953c0067f0de8e827d43209800d085a
Marek <jeffyakametin2@gmail.com>
2022-07-26 17:43:09 +0200

    Changing linux distro

--- infrastructure/terraform/ec2_instance.tf
+++ infrastructure/terraform/ec2_instance.tf
@@ -1,6 +1,6 @@
 # this recource will create a ec2 instance with specified specs
 resource "aws_instance" "ec2_instance" {
-  ami                    = "ami-0a1ee2fb28fe05df3"            # this is a amazon linux ami id
+  ami                    = "ami-065deacbcaac64cf2"            # this is a Ubuntu linux ami id
   instance_type          = "t2.micro"
   security_groups = [aws_security_group.security_group.name]
   tags = {
