bb04dd9886c0411200e86894d6db8c9aaed88bda
Marek <jeffyakametin2@gmail.com>
2022-07-26 15:11:14 +0200

    Adding ec2 instance

---
+++ infrastructure/terraform/ec2_instance.tf
@@ -0,0 +1,10 @@
+# this recource will create a ec2 instance with specified specs
+resource "aws_instance" "ec2_instance" {
+  ami                    = "ami-0a1ee2fb28fe05df3"            # this is a amazon linux ami id
+  instance_type          = "t2.micro"
+  key_name               = var.key_name
+  security_groups = [aws_security_group.security_group.name]
+  tags = {
+    Name = "webServer"
+  }
+}
