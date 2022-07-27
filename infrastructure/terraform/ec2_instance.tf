59fb4c61d76beaec5544eb4e4bb6b4c301851652
Marek <jeffyakametin2@gmail.com>
2022-07-27 17:32:12 +0200

    Adding key name

--- infrastructure/terraform/ec2_instance.tf
+++ infrastructure/terraform/ec2_instance.tf
@@ -3,6 +3,7 @@ resource "aws_instance" "ec2_instance" {
   ami                    = "ami-065deacbcaac64cf2"            # this is a Ubuntu linux ami id
   instance_type          = "t2.micro"
   security_groups = [aws_security_group.security_group.name]
+  key_name = "admin"
   tags = {
     Name = "webServer"
   }
