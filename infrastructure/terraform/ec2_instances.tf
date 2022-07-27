422a2ca264e1a3ed53db734dc3bffe4edb2538ee
Marek <jeffyakametin2@gmail.com>
2022-07-27 21:00:46 +0200

    Remove unnecessary ec2 instance for Jenkins

--- infrastructure/terraform/ec2_instances.tf
+++ infrastructure/terraform/ec2_instances.tf
@@ -8,14 +8,3 @@ resource "aws_instance" "ec2_instance_WebServer" {
     Name = "WebServer"
   }
 }
-
-# this recource will create a ec2 instance with specified specs
-resource "aws_instance" "ec2_instance_Jenkins" {
-  ami                    = "ami-065deacbcaac64cf2"            # this is a Ubuntu linux ami id
-  instance_type          = "t2.micro"
-  security_groups = [aws_security_group.security_group.name]
-  key_name = "admin"
-  tags = {
-    Name = "Jenkins"
-  }
-}
