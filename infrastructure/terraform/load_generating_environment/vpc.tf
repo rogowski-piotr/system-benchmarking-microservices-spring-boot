d45c0b2bdd75912c10df9da0f696cfab35627f47
Marek <jeffyakametin2@gmail.com>
2022-09-27 20:31:44 +0200

    Removal of unnecessary workflow

--- infrastructure/terraform/load_generating_environment/vpc.tf
+++
@@ -1,9 +0,0 @@
-# this is default vpc in aws ,
-# this niether be created during terraform apply and nor be destroyed during terraform destroy
-# this the just for referncing attribute of default vpc
-
-resource "aws_default_vpc" "default_vpc" {
-  tags = {
-    Name = "Default VPC"
-  }
-}
