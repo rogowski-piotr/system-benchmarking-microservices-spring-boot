16307a4b94761ebb7c0b6d4b19200d8035d0c21b
Marek <jeffyakametin2@gmail.com>
2022-09-29 22:29:05 +0200

    Edit path

--- scripts/decryption/decrypt_secret.sh
+++ scripts/decryption/decrypt_secret.sh
@@ -5,4 +5,4 @@ mkdir $HOME/secrets
 # --batch to prevent interactive command
 # --yes to assume "yes" for questions
 gpg --quiet --batch --yes --decrypt --passphrase="$PASSWORD" \
---output $HOME/secrets/admin.pem infrastructure/ssh_key/admin.pem.gpg
+--output $HOME/secrets/admin.pem ./../../infrastructure/ssh_key/admin.pem.gpg
