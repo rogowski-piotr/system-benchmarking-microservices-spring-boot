a307384711cea6354e1d8eae8ee5ce18b0113ede
Marek <jeffyakametin2@gmail.com>
2022-09-29 22:51:52 +0200

    Edit scirpt

--- scripts/decryption/decrypt_secret.sh
+++ scripts/decryption/decrypt_secret.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 
 # Decrypt the file
-mkdir $HOME/secrets
-# --batch to prevent interactive command
-# --yes to assume "yes" for questions
-gpg --quiet --batch --yes --decrypt --passphrase="$PASSWORD" \
---output $HOME/secrets/admin.pem ./infrastructure/ssh_key/admin.pem.gpg
+gpg --quiet --decrypt --passphrase=""$PASSWORD"" ./infrastructure/ssh_key/admin.pem.gpg > .ssh/admin.pem
+
+
