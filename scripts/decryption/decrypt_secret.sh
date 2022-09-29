7011850327e694d33d41cda261edaeaeee45d0bf
Marek <jeffyakametin2@gmail.com>
2022-09-29 23:20:31 +0200

    Edit scirpt

--- scripts/decryption/decrypt_secret.sh
+++ scripts/decryption/decrypt_secret.sh
@@ -2,7 +2,6 @@
 
 # Decrypt the file
 cd /infrastructure/ssh_key
-gpg --quiet --decrypt --batch --yes --passphrase="test" admin.pem.gpg > admin.pem
-cd
-mv ./infrastructure/ssh_key/admin.pem ./../../.ssh/admin.pem
+gpg --quiet --decrypt --batch --yes --passphrase="test" ./infrastructure/ssh_key/admin.pem.gpg > admin.pem
+mv admin.pem .ssh/admin.pem
 
