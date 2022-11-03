f5d541e073e72cb4db675b7c1005def1451ef443
Piotr Rogowski <62942330+rogowski-piotr@users.noreply.github.com>
2022-11-03 10:27:47 +0100

    Fix host address in collecting data stage

--- scripts/benchmark-env/run_benchmark.sh
+++ scripts/benchmark-env/run_benchmark.sh
@@ -56,10 +56,10 @@ collectDataFunction()
     containers=$(docker ps --format {{.Names}})
 
     for container_name in $containers; do
-        curl --location -g --request GET "http://localhost:9090/api/v1/query_range?query=rate(container_cpu_usage_seconds_total{name=\"$container_name\"}[5m])*100&start=$startTimestamp&end=$finishTimestamp&step=5s" > output/${file}/${container_name}_percentage_cpu_usage_seconds_total.json
-        curl --location -g --request GET "http://localhost:9090/api/v1/query_range?query=rate(container_memory_usage_bytes{name=\"$container_name\"}[5m])&start=$startTimestamp&end=$finishTimestamp&step=5s" > output/${file}/${container_name}_memory_usage_bytes.json
-        curl --location -g --request GET "http://localhost:9090/api/v1/query_range?query=rate(container_network_receive_bytes_total{name=\"$container_name\"}[5m])&start=$startTimestamp&end=$finishTimestamp&step=5s" > output/${file}/${container_name}_network_receive_bytes_total.json
-        curl --location -g --request GET "http://localhost:9090/api/v1/query_range?query=rate(container_network_transmit_bytes_total{name=\"$container_name\"}[5m])&start=$startTimestamp&end=$finishTimestamp&step=5s" > output/${file}/${container_name}_network_transmit_bytes_total.json
+        curl --location -g --request GET "http://${load_generating_host}:9090/api/v1/query_range?query=rate(container_cpu_usage_seconds_total{name=\"$container_name\"}[5m])*100&start=$startTimestamp&end=$finishTimestamp&step=5s" > output/${file}/${container_name}_percentage_cpu_usage_seconds_total.json
+        curl --location -g --request GET "http://${load_generating_host}:9090/api/v1/query_range?query=rate(container_memory_usage_bytes{name=\"$container_name\"}[5m])&start=$startTimestamp&end=$finishTimestamp&step=5s" > output/${file}/${container_name}_memory_usage_bytes.json
+        curl --location -g --request GET "http://${load_generating_host}:9090/api/v1/query_range?query=rate(container_network_receive_bytes_total{name=\"$container_name\"}[5m])&start=$startTimestamp&end=$finishTimestamp&step=5s" > output/${file}/${container_name}_network_receive_bytes_total.json
+        curl --location -g --request GET "http://${load_generating_host}:9090/api/v1/query_range?query=rate(container_network_transmit_bytes_total{name=\"$container_name\"}[5m])&start=$startTimestamp&end=$finishTimestamp&step=5s" > output/${file}/${container_name}_network_transmit_bytes_total.json
     done
 
     curl --location -g --request GET "http://localhost:9090/api/v1/query_range?query=sum(rate(container_cpu_user_seconds_total{image!=\"\"}[5m])*100)&start=$startTimestamp&end=$finishTimestamp&step=5s" > output/${file}/sum_percentage_cpu_usage_seconds_total.json
