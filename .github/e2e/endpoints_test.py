8bc5aedf47afa20b31b2e44bb5d73de3723acaf9
Piotr Rogowski <rogowski.piotr99@gmail.com>
2023-02-08 21:05:58 +0100

    adjust e2e tests

--- .github/e2e/endpoints_test.py
+++ .github/e2e/endpoints_test.py
@@ -7,7 +7,7 @@ def test_place_service():
     host = "localhost"
     port = "8080"
     endpoint = "/api/places"
-    params = ""
+    params = "?ids=1,22,333,1000"
     url = f"http://{host}:{port}{endpoint}{params}"
     payload = {}
     headers = {}
@@ -18,7 +18,7 @@ def test_place_service():
     # then
     assert response.status_code == 200, f"Unexpected response code {response.status_code}, expected 200"
     places_amount_in_response = len(json.loads(response.text))
-    expected_places_amount = 2318
+    expected_places_amount = 4
     assert places_amount_in_response == expected_places_amount, \
         f"Expected {expected_places_amount} places, found {places_amount_in_response}"
 
@@ -30,22 +30,46 @@ def test_distance_service():
     endpoint = "/api/distance"
     params = ""
     url = f"http://{host}:{port}{endpoint}{params}"
-    payload = json.dumps({
-        "coordinate1": "54.367, 18.633",
-        "coordinate2": "50.05, 19.95"
-    })
+    payload = json.dumps([
+        {
+            "placeId1": 1,
+            "placeId2": 2,
+            "coordinates1": "54.367, 18.633",
+            "coordinates2": "50.05, 19.95"
+        },
+        {
+            "placeId1": 3,
+            "placeId2": 4,
+            "coordinates1": "55.367, 19.633",
+            "coordinates2": "52.05, 14.95"
+        },
+        {
+            "placeId1": 1,
+            "placeId2": 3,
+            "coordinates1": "54.367, 18.633",
+            "coordinates2": "55.367, 19.633"
+        }
+    ])
     headers = {"Content-Type": "application/json"}
 
     # when
     response = requests.request("POST", url, headers=headers, data=payload)
 
     # then
-    expected_distance = 488
-    distance_value_in_response = int(json.loads(response.text)["distance"])
     assert response.status_code == 200, \
-    f"Unexpected response code {response.status_code}, expected 200"
-    assert distance_value_in_response == expected_distance, \
-        f"Bad distance ({str.strip(response.text)}), expected {expected_distance}"
+        f"Unexpected response code {response.status_code}, expected 200"
+    
+    expected_distances_elements = 3
+    expected_distance_value_at_first_element = 488
+
+    json_response = json.loads(response.text)
+    distances_amount_in_response = int(len(json_response))
+    distance_value_at_first_element = int(json_response[0]["distance"])
+    
+    assert distances_amount_in_response == expected_distances_elements, \
+        f"Bad distances amount in response list: ({str.strip(response.text)}), expected {expected_distances_elements}"
+    assert distance_value_at_first_element == expected_distance_value_at_first_element, \
+        f"Bad distances value in first element of response list: ({str.strip(response.text)}), expected {expected_distance_value_at_first_element}"
 
 
 def test_route1_service():
