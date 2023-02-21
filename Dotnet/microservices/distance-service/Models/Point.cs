7eedf1cca788f65c18a536dc4cb3f5a5c15aa713
Piotr Rogowski <rogowski.piotr99@gmail.com>
2023-02-21 10:17:35 +0100

    optimize dotnet distance service

--- Dotnet/microservices/distance-service/Models/Point.cs
+++ Dotnet/microservices/distance-service/Models/Point.cs
@@ -2,15 +2,19 @@
 {
     public class Point
     {
-        public double Latitude { get; set; }
-        public double Longitude { get; set; }
+        public int PlaceId1 { get; set; }
+        public int PlaceId2 { get; set; }
+        public double Latitude1 { get; set; }
+        public double Longitude1 { get; set; }
+        public double Latitude2 { get; set; }
+        public double Longitude2 { get; set; }
 
         public Point()
         {
 
         }
 
-        public Point(double lat, double lon)
-            => (Latitude, Longitude) = (lat, lon);
+        public Point(int id1, int id2, double lat1, double lon1, double lat2, double lon2)
+            => (PlaceId1, PlaceId2, Latitude1, Longitude1, Latitude2, Longitude2) = (id1, id2, lat1, lon1, lat2, lon2);
     }
 }
--- Dotnet/microservices/distance-service/Responses/DistanceResponse.cs
+++ Dotnet/microservices/distance-service/Responses/DistanceResponse.cs
@@ -1,15 +1,35 @@
-﻿namespace DistanceService.Responses
+﻿using System.Text.Json.Serialization;
+using DistanceService.Models;
+
+namespace DistanceService.Responses
 {
     public class DistanceResponse
     {
-        public double Distance { get; private set; }
+        [JsonPropertyName("placeId1")]
+        public int PlaceId1 { get; set; }
 
-        private DistanceResponse(double distance)
+        [JsonPropertyName("placeId2")]
+        public int PlaceId2 { get; set; }
+
+        [JsonPropertyName("distance")]
+        public double Distance { get; set; }
+
+        private DistanceResponse(int placeId1, int placeId2, double distance)
         {
+            PlaceId1 = placeId1;
+            PlaceId2 = placeId2;
             Distance = distance;
         }
 
-        public static DistanceResponse EntityToDtoMapper(double distance)
-            => new(distance);
+        public static List<DistanceResponse> EntityToDtoMapper(List<Point> points, List<double> distances)
+        {
+            List<DistanceResponse> distanceResponseList = new List<DistanceResponse>();
+            for (int iter = 0; iter < points.Count; iter++)
+            {
+                distanceResponseList.Add(
+                    new DistanceResponse(points[iter].PlaceId1, points[iter].PlaceId2, distances[iter]));
+            }
+            return distanceResponseList;
+        }
     }
 }
