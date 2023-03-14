a8f35e7cdf0f06dd0908ee0934eea6d207344fcc
Piotr Rogowski <rogowski.piotr99@gmail.com>
2023-03-14 18:27:13 +0100

    Add customization for number of threads pool for flask

--- Flask/microservices/route1-service/static/config.py
+++ Flask/microservices/route1-service/static/config.py
@@ -5,6 +5,7 @@ HOST='0.0.0.0'
 PORT=8083
 DEBUG=False
 PROD_ENV=True
+THREAD_POOL_SIZE=3
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 
--- Flask/monolith/static/config.py
+++ Flask/monolith/static/config.py
@@ -5,4 +5,5 @@ HOST='0.0.0.0'
 PORT=8080
 DEBUG=False
 PROD_ENV=True
+THREAD_POOL_SIZE=3
 basedir = os.path.abspath(os.path.dirname(__file__))
