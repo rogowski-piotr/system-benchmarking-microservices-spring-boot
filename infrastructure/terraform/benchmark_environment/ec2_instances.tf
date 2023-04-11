8b62cbdd21a962624e0e47e041743872642d2dfb
Piotr Rogowski <62942330+rogowski-piotr@users.noreply.github.com>
2023-04-11 11:42:44 +0200

    temporary add two aws instances at terraform

--- infrastructure/terraform/benchmark_environment/ec2_instances.tf
+++ infrastructure/terraform/benchmark_environment/ec2_instances.tf
@@ -9,6 +9,16 @@ resource "aws_instance" "ec2_instance_Benchmark_environment" {
   }
 }
 
+resource "aws_instance" "ec2_instance_Benchmark_environment_1" {
+  ami                    = "ami-065deacbcaac64cf2"            # this is a Ubuntu linux ami id
+  instance_type          = "INSTANCE_TYPE"                    # this is a param for github workflow
+  security_groups = [aws_security_group.security_group.name]
+  key_name = "admin"
+  tags = {
+    Name = "Benchmark environment 1"
+  }
+}
+
 # this recource will create a ec2 instance with specified specs
 resource "aws_instance" "ec2_instance_Load_generating_environment" {
   ami                    = "ami-065deacbcaac64cf2"                  # this is a Ubuntu linux ami id     
